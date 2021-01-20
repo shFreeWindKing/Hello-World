42522d413048ef38bf792e648df4e0a4c7ce5170
shFreeWindKing <djian_1981@163.com>
2021-01-20 09:12:45 +0800

    mybatis配置及相关注释

--- eastdemo/src/main/java/com/shptchangfeng/eastdemo/config/datasource/MysqlDruidDataSourceConfig.java
+++ eastdemo/src/main/java/com/shptchangfeng/eastdemo/config/datasource/MysqlDruidDataSourceConfig.java
@@ -23,11 +23,17 @@ public class MysqlDruidDataSourceConfig {
     static final String MAPPER_PACKAGE = "com.shptchangfeng.eastdemo.dao";
     //自定义mapper的xml文件路径
     private static final String MAPPER_XML_PATH = "classpath:/mysql-mapper/*.xml";
+    //自定义mybatis.config-locations
+    private static final String MYBATIS_CONFIG_PATH = "classpath:/mybatis/mybatis-config.xml";
     //数据源配置的前缀，必须与application.properties中配置的对应数据源的前缀一致
     private static final String DATASOURCE_PREFIX = "spring.datasource";
     //druid的log Filter配置
     private static final String LOFFILTER_PREFIX = "druid";
     private static final String  SLF4JLOGFILTER = "my-log-filter";
+
+    //mybatis.type-aliases-package
+    //使用type-aliases-package中指定entity扫描包类让mybatis自定扫描到自定义的entity
+    private  static final String TYPE_ALIASES_PACKAGE = "com.shptchangfeng.eastdemo.po";
 
     @Primary
     @Bean(name = DATASOURCE_NAME)
@@ -45,6 +51,8 @@ public class MysqlDruidDataSourceConfig {
             PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
             //自定义mapper的xml文件地址，当通用mapper提供的默认功能无法满足我们的需求时，可以自己添加实现，与mybatis写mapper一样
             sessionFactoryBean.setMapperLocations(resolver.getResources(MAPPER_XML_PATH));
+            sessionFactoryBean.setConfigLocation(resolver.getResource(MYBATIS_CONFIG_PATH));
+            sessionFactoryBean.setTypeAliasesPackage(TYPE_ALIASES_PACKAGE);
             return sessionFactoryBean.getObject();
         } catch (Exception e) {
             log.error("配置demo的SqlSessionFactory失败，error:{}", e.getMessage());
